e609871a2656d2d2b4d4b20f6067e3d996245e27
Steffield <steffilebby@gmail.com>
2020-05-03 16:23:45 -0400

    accidentally pushed an old server file with the style changes

--- server.js
+++ server.js
@@ -3,13 +3,11 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3006 || process.env.PORT;
+const PORT = process.env.PORT || 3008 ;
 
 const app = express();
 
 app.use(logger("dev"));
-
-
 
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
@@ -18,7 +16,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true //added to not throw error
